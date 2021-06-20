0c5e2b96a878942c425707a1ffea695387f4d438
codingCat-dev <85181534+codingCat-dev@users.noreply.github.com>
2021-06-20 14:01:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -11,56 +11,14 @@ const form = document.querySelector('.form');
 const formText = document.getElementById('text');
 const formAmount = document.getElementById('amount');
 
-let expenses = [];
-const generateRandomId = () => {
-  return Math.floor(Math.random() * 500);
-};
+const localStorageExpenses = JSON.parse(localStorage.getItem('expenses'));
 
-const generateDom = transaction => {
-  const sign = transaction.amount < 0 ? '-' : '+';
-
-  const listItem = document.createElement('li');
-  listItem.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
-
-  listItem.innerHTML = `
-    ${transaction.text} <span>${sign}${Math.abs(
-    transaction.amount
-  )}</span> <button class="delete-btn btn" onclick="deleteExpense(${
-    transaction.id
-  })">x</button>
-  `;
-  list.appendChild(listItem);
-};
-
-const updateValues = () => {
-  const amounts = expenses.map(expense => expense.amount);
-  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
-  const income = amounts
-    .filter(item => item > 0)
-    .reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
-
-  const expense = (
-    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
-    -1
-  ).toFixed(2);
-
-  balance.innerText = `${total}`;
-  incomeMoney.innerText = `${income}`;
-  expenseMoney.innerText = `${expense}`;
-};
-
-const deleteExpense = () => {
-  list.innerHTML = '';
-  updateValues();
-};
-
-const updateError = message => {
-  error.textContent = message;
-};
+let expenses =
+  localStorage.getItem('expenses') != null ? localStorageExpenses : [];
 
 const addAction = e => {
   e.preventDefault();
+
   if (formText.value.trim() === '' && formAmount.value.trim() === '') {
     updateError('Please enter name and amount');
   }
@@ -76,11 +34,77 @@ const addAction = e => {
       amount: +formAmount.value,
     };
     expenses.push(newAction);
+
     generateDom(newAction);
+
     updateValues();
+
+    updateLocalStorage();
+
     formText.value = '';
     amount.value = '';
   }
+};
+
+const generateRandomId = () => {
+  return Math.floor(Math.random() * 500);
+};
+
+const generateDom = transaction => {
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  const listItem = document.createElement('li');
+
+  listItem.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+  listItem.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span> <button class="delete-btn btn" onclick="deleteExpense(${
+    transaction.id
+  })">x</button>
+  `;
+  list.appendChild(listItem);
+};
+
+const updateValues = () => {
+  const amounts = expenses.map(expense => expense.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expenseItem = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  balance.innerText = `${total}`;
+  incomeMoney.innerText = `${income}`;
+  expenseMoney.innerText = `${expenseItem}`;
+};
+
+const deleteExpense = id => {
+  expenses = expenses.filter(expense => expense.id !== id);
+  updateLocalStorage();
+  update();
+};
+
+const updateLocalStorage = () => {
+  localStorage.setItem('expenses', JSON.stringify(expenses));
+};
+const update = () => {
+  list.innerHTML = '';
+  expenses.forEach(generateDom);
+  updateValues();
+};
+update();
+
+const updateError = message => {
+  error.textContent = message;
 };
 
 form.addEventListener('submit', addAction);
